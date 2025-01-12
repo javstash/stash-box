3a1961296aa8e5b3381afe00a9ba560fc2538f7f
javstash <admin@javstash.org>
2025-01-12 09:06:41 -0500

    Add performer name to SceneCard

--- frontend/src/components/sceneCard/SceneCard.tsx
+++ frontend/src/components/sceneCard/SceneCard.tsx
@@ -7,15 +7,16 @@ import { Scene, Studio } from "src/graphql";
 import {
   getImage,
   sceneHref,
+  performerHref,
   studioHref,
   formatDuration,
   imageType,
 } from "src/utils";
-import { Icon, Thumbnail } from "src/components/fragments";
+import { Icon, Thumbnail, PerformerName } from "src/components/fragments";
 
 type Performance = Pick<
   Scene,
-  "id" | "title" | "images" | "duration" | "code" | "release_date"
+  "id" | "title" | "images" | "duration" | "code" | "release_date" | "performers"
 > & {
   studio?: Pick<Studio, "id" | "name"> | null;
 };
@@ -24,50 +25,68 @@ const CLASSNAME = "SceneCard";
 const CLASSNAME_IMAGE = `${CLASSNAME}-image`;
 const CLASSNAME_BODY = `${CLASSNAME}-body`;
 
-const SceneCard: FC<{ scene: Performance }> = ({ scene }) => (
-  <Card className={CLASSNAME}>
-    <Card.Body className={CLASSNAME_BODY}>
-      <Link className={CLASSNAME_IMAGE} to={sceneHref(scene)}>
-        <Thumbnail
-          alt={scene.title}
-          className={imageType(scene.images[0])}
-          image={getImage(scene.images, "landscape")}
-          size={300}
-        />
-      </Link>
-    </Card.Body>
-    <Card.Footer>
-      <div className="d-flex">
-        <Link
-          className="text-truncate w-100"
-          to={sceneHref(scene)}
-          title={scene.title ?? ""}
-        >
-          <h6 className="text-truncate">{scene.title}</h6>
-        </Link>
-      </div>
-      <div className="text-muted">
-        <Link className="text-truncate w-100" to={sceneHref(scene)}>
-          <strong>{scene.code}</strong>
-        </Link>
-        <span className="text-muted float-end">
-          {scene.duration ? formatDuration(scene.duration) : ""}
-        </span>
-      </div>
-      <div className="text-muted">
-        {scene.studio && (
-          <Link
-            to={studioHref(scene.studio)}
-            className="float-end text-truncate SceneCard-studio-name"
-          >
-            <Icon icon={faVideo} className="me-1" />
-            {scene.studio.name}
-          </Link>
-        )}
-        <strong>{scene.release_date}</strong>
-      </div>
-    </Card.Footer>
-  </Card>
-);
+const SceneCard: FC<{ scene: Performance }> = ({ scene }) => {
 
+  const performers = scene.performers
+    .map((performance) => {
+      const { performer } = performance;
+      return (
+        <Link
+          key={performer.id}
+          to={performerHref(performer)}
+          className="scene-performer"
+        >
+          <PerformerName performer={performer} />
+        </Link>
+      );
+    })
+    .map((p, index) => (index % 2 === 2 ? [" • ", p] : p));
+
+    return (
+    <Card className={CLASSNAME}>
+      <Card.Body className={CLASSNAME_BODY}>
+        <Link className={CLASSNAME_IMAGE} to={sceneHref(scene)}>
+          <Thumbnail
+            alt={scene.title}
+            className={imageType(scene.images[0])}
+            image={getImage(scene.images, "landscape")}
+            size={300}
+          />
+        </Link>
+      </Card.Body>
+      <Card.Footer>
+        <div className="d-flex">
+          <Link
+            className="text-truncate w-100"
+            to={sceneHref(scene)}
+            title={scene.title ?? ""}
+          >
+            <h6 className="text-truncate">{scene.title}</h6>
+          </Link>
+        </div>
+        <div className="text-truncate w-100 scene-performers me-auto">{performers}</div>
+        <div className="text-muted">
+          <Link className="text-truncate w-100" to={sceneHref(scene)}>
+            <strong>{scene.code}</strong>
+          </Link>
+          <span className="text-muted float-end">
+            {scene.duration ? formatDuration(scene.duration) : ""}
+          </span>
+        </div>
+        <div className="text-muted">
+          {scene.studio && (
+            <Link
+              to={studioHref(scene.studio)}
+              className="float-end text-truncate SceneCard-studio-name"
+            >
+              <Icon icon={faVideo} className="me-1" />
+              {scene.studio.name}
+            </Link>
+          )}
+          <strong>{scene.release_date}</strong>
+        </div>
+      </Card.Footer>
+    </Card>
+  );
+};
 export default SceneCard;
