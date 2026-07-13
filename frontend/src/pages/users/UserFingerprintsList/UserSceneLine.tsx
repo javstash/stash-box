1e95a4712a0d5bf8334f6fb3ad26c855dd220860
javstash <admin@javstash.org>
2026-07-13 18:27:14 -0400

    Add sort by code to UserFingerprintsList

--- frontend/src/pages/users/UserFingerprintsList/UserSceneLine.tsx
+++ frontend/src/pages/users/UserFingerprintsList/UserSceneLine.tsx
@@ -46,6 +46,7 @@ const UserSceneLine: FC<Props> = ({ scene, deleteFingerprints }) => (
       <td>
         <Link to={sceneHref(scene)}>{scene.title}</Link>
       </td>
+      <td>{scene.code}</td>
       <td>
         {scene.studio && (
           <Link
